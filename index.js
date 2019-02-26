589f302af1ac89d9578e4fc0a229472f2e58c18c
Spencer Lindemuth <lindemut@gmail.com>
2019-02-26 18:38:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -88,6 +88,6 @@ function placeOrder(cardNumber) {
   else{
     cartTotal = total()
     cart = []
-    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+    return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
   }
 }
