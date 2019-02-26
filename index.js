371f7873e2a677cc1ef11341906c0f7b1468e6f3
Spencer Lindemuth <lindemut@gmail.com>
2019-02-26 18:38:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else{
-    cartTotal = total()
+    var cartTotal = total()
     cart = []
     return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`
   }
