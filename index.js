731511474f964afd2ce22c04057927605000742d
Spencer Lindemuth <lindemut@gmail.com>
2019-02-26 18:05:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function viewCart() {
   else if(cart.length === 2){
     var bar = cart[0]
     var foo = cart[1]
-    var str = `In your cart, you have ${bar.itemname} at $${bar.itemPrice}, and ${foo.itemName} batter at $${foo.itemPrice}.`
+    var str = `In your cart, you have ${bar.itemName} at $${bar.itemPrice}, and ${foo.itemName} batter at $${foo.itemPrice}.`
     return str
   }
 }
